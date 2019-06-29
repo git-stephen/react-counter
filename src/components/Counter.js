181d0985d7e635c93a30402dc049045f2077e9af
git-stephen <stephen280290@gmail.com>
2019-06-29 18:57:08 +0530

    counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class Counter extends Component
+{
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            count:0
+        }
+    }
+    
+    IncreaseCount()
+    {
+        this.setState(
+            count = this.state.count + 1
+        )
+    }
+    render() {
+        return (
+            <div>
+                <h1> This is the counter application </h1>
+                <button onClick={()=> this.IncreaseCount() }> Counter </button>
+                <p>Total number of clicks is {this.state.count}</p>
+            </div>
+        )
+    }
+}
+
+export default Counter
